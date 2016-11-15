fa697061e86c807892bb1b685d2f9f9ec7ea3d55
rancheng <rancheng@ucfgroup.com>
2016-11-15 16:56:40 +0800

    录音机

---
+++ record/webServer.js
@@ -0,0 +1,29 @@
+var https = require('https');
+var fs = require('fs');
+var url = require('url');
+
+
+var root = './root/';
+function getFullPath(path){
+    return root+path;
+};
+
+
+var options = {
+    pfx:fs.readFileSync('./SSL/server.pfx'),
+    passphrase:'123321'
+};
+
+
+
+https.createServer(options,function(req,res){
+
+
+    //根据路径字符串  展示路径所有信息
+    var urlObj = url.parse(req.url,true);
+
+    var resFile = fs.readFileSync( getFullPath(urlObj.pathname) );
+
+    res.writeHead(200);
+    res.end(resFile);
+}).listen(3000,'127.0.0.1');
